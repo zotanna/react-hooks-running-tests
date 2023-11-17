60fea52d72ccef16fbcdefa5129fac585391a825
denis <denniiskim@gmail.com>
2023-11-17 03:56:27 +0300

    completed assignment

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,11 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import Article from "../components/Article";
+function Article() {
+  return (
+    <div>
+      please pass this test
+    </div>
+  );
+}
 
-test("displays the text 'please pass this test'", () => {
-  render(<Article />);
-
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
-});
+export default Article;
--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,11 +1,11 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import Header from "../components/Header";
+function Header(props) {
+      return (
+        <h1>
+          hello from the Header!
+        </h1>
+      )
+}
 
-test("displays the text 'hello from the Header!'", () => {
-  render(<Header />);
-
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
-});
+export default Header;
